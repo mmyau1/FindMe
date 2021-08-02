d7ae17d711ef615b8bcefa2c9103777260e0db69
mmyau1 <86981889+mmyau1@users.noreply.github.com>
2021-08-02 16:04:05 -0500

    Find Me
    Map Kit with user location and search button using MK Search finding nearby hospitals

--- FindMe/ViewController.swift
+++ FindMe/ViewController.swift
@@ -6,14 +6,67 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func searchButton(_ sender: Any) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = "Hospital"
+        searchRequest.region = mapView.region
+        
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            if let response = response {
+                for mapItem in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.coordinate = mapItem.placemark.coordinate
+                    annotation.title = mapItem.placemark.name
+                    annotation.subtitle = mapItem.placemark.title
+                    self.mapView.addAnnotation(annotation)
+                }
+            }
+        }
+    }
+    
+    let manager = CLLocationManager()
+    var currentLocation = CLLocation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userLocation.title = "My Location"
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            currentLocation = location
+            
+            render(location)
+        }
+        func render(_ location: CLLocation) {
+            let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            
+            let region = MKCoordinateRegion(center: coordinate, span: span)
+            
+            mapView.setRegion(region, animated: true)
+            
+        }
     }
 
+    
+    
 
 }
 
